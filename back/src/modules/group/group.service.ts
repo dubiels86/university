a49cdf36643fce6687e0cb4cafe11f3269b2ce64
Dubiel Santana <dubiels86@gmail.com>
2021-08-24 23:00:26 -0400

    Back: create group service crud

--- back/src/modules/group/group.service.ts
+++ back/src/modules/group/group.service.ts
@@ -1,4 +1,83 @@
-import { Injectable } from '@nestjs/common';
+import {
+    BadRequestException,
+    HttpException,
+    HttpStatus,
+    Injectable,
+    NotFoundException,
+  } from "@nestjs/common";
+  import { InjectRepository } from "@nestjs/typeorm";
+  import { getManager } from "typeorm";
+  import { Group } from "../entities/group.entity";
+  import { GroupDto } from "./dto/group.dto";
+  import { GroupRepository } from "./group.repository";
+  import { status } from "../shared/entity-status.enum";
+  import { plainToClass } from "class-transformer";
 
 @Injectable()
-export class GroupService {}
+export class GroupService {
+    manager = getManager();
+  constructor(
+    @InjectRepository(GroupRepository)
+    private readonly _groupRepository: GroupRepository
+  ) {}
+
+  async getOne(id: number): Promise<GroupDto> {
+    if (!id) {
+      throw new BadRequestException("id must be sent");
+    }
+
+    const group: Group = await this._groupRepository.findOne(id, {
+      where: { status: status.ACTIVE },
+    });
+
+    if (!group) {
+      throw new NotFoundException();
+    }
+
+    return plainToClass(GroupDto, group);
+  }
+
+  async getAll(): Promise<GroupDto[]> {
+    const groups: Group[] = await this._groupRepository.find({
+      where: { status: status.ACTIVE },
+    });
+
+    return groups.map((g: Group) => plainToClass(GroupDto, g));
+  }
+
+  async create(g: Group) {
+    const p = await this._groupRepository.findOne({ name: g.name });
+    if (p) {
+      throw new HttpException("El grupo ya existe.", HttpStatus.CONFLICT);
+    }
+    await this._groupRepository.save(g);
+  }
+
+  async update(id: number, g: Partial<GroupDto>): Promise<GroupDto> {
+    const foundGroup: Group = await this._groupRepository.findOne(id, {
+      where: { status: status.ACTIVE },
+    });
+    if (!foundGroup) {
+      throw new NotFoundException("This Group does not exist");
+    }
+
+    foundGroup.name = g.name;
+    foundGroup.teacher = g.teacher;
+    
+    const savedGroup = await this._groupRepository.save(foundGroup);
+
+    return plainToClass(GroupDto, savedGroup);
+  }
+
+  async delete(id: number): Promise<void> {
+    const groupExists = await this._groupRepository.findOne(id, {
+      where: { status: status.ACTIVE },
+    });
+
+    if (!groupExists) {
+      throw new NotFoundException();
+    }
+
+    await this._groupRepository.update(id, { status: status.INACTIVE });
+  }
+}
