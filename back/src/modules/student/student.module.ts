d19a8c055c9ba603b9b220b60bf561a988bbdab7
Dubiel Santana <dubiels86@gmail.com>
2021-08-24 22:37:17 -0400

    Back: create student module

--- back/src/modules/student/student.module.ts
+++ back/src/modules/student/student.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { StudentController } from './student.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StudentRepository } from './student.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([StudentRepository])],
   providers: [StudentService],
   controllers: [StudentController]
 })
