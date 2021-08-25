21c9dea8c1490d0abddfcb417e841c5c6e94fccf
Dubiel Santana <dubiels86@gmail.com>
2021-08-25 00:05:04 -0400

    Front: create student & group models

--- front/src/app/config/config.ts
+++ front/src/app/config/config.ts
@@ -1,6 +1,4 @@
 export const Configuration = {
-	//apiEndpoint: 'https://192.168.132.173:8443/portal-it/'
-	apiEndpoint: 'https://localhost:3000/portal-it/',
-	apiEndpointBase: 'https://localhost:3001/api',
-	apiEndpointKeyInHand: 'http://localhost:3000/api'
+	
+	apiEndpoint: 'https://localhost:3000/api',
 };
---
+++ front/src/app/model/student.entity.ts
@@ -0,0 +1,13 @@
+export class Student {
+    id?: number;
+    name?: string;
+    age?: number;
+    sex?: string;
+    email?: string;
+    bithday?: string;
+    place_birth?: string;
+    group?: any;
+    
+    
+    
+}
