9d4d3c91b2beb076c061eb3bd1c231f199cbd3a2
Dubiel Santana <dubiels86@gmail.com>
2021-08-24 21:48:24 -0400

    Back: create group repository

---
+++ back/src/modules/group/group.repository.ts
@@ -0,0 +1,6 @@
+import { Repository, EntityRepository } from "typeorm";
+import { Group } from "../entities/group.entity";
+
+
+@EntityRepository(Group)
+export class GroupRepository extends Repository<Group>{}
